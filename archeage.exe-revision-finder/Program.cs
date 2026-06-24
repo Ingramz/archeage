42ce5784ab809aecc74a65871228ab5926ee220b
Indrek Ardel <indrek@ardel.eu>
2026-06-24 15:20:11 +0300

    Update revision finder to find the `game.sqlite3` key

--- archeage.exe-revision-finder/Program.cs
+++ archeage.exe-revision-finder/Program.cs
@@ -1,12 +1,13 @@
-using System;
-using System.IO;
-using System.Reflection.PortableExecutable;
+﻿using System.Reflection.PortableExecutable;
+using System.Security.Cryptography;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        string filePath = @"C:\KakaoGames\ArcheAge\bin64\archeage.exe";
+        string filePath = @"bin64\archeage.exe";
+        string dbPath = @"game\db\game.sqlite3";
+        string destination = @"game\db\game_decrypted.sqlite3";
 
         try
         {
@@ -22,14 +23,80 @@ class Program
             stream.Seek(ptr, SeekOrigin.Begin);
 
             byte[] int32 = new byte[4];
-
             stream.ReadExactly(int32);
 
-            Console.WriteLine(BitConverter.ToUInt32(int32));
+            Console.WriteLine("Revision: " + BitConverter.ToUInt32(int32));
+
+            stream.Seek(68, SeekOrigin.Current);
+
+            byte[] sKey = new byte[16];
+            stream.ReadExactly(sKey);
+
+            Console.WriteLine("Key: " + Convert.ToHexString(sKey));
+
+            decryptDatabase(dbPath, destination, sKey);
+            //encryptDatabase(destination, dbPath + ".new", sKey);
         }
         catch (Exception ex)
         {
             Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+
+    static void decryptDatabase(string src, string dst, byte[] key)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = key;
+            aes.IV = new byte[16];
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.None;
+
+            using (FileStream fsInput = new FileStream(src, FileMode.Open, FileAccess.Read))
+            using (FileStream fsOutput = new FileStream(dst, FileMode.Create, FileAccess.Write))
+            using (ICryptoTransform decryptor = aes.CreateDecryptor())
+            using (CryptoStream cs = new CryptoStream(fsInput, decryptor, CryptoStreamMode.Read))
+            {
+                cs.CopyTo(fsOutput);
+            }
+        }
+
+        Console.WriteLine("Decryption did not return any errors.");
+
+        using (FileStream fsInput = new FileStream(dst, FileMode.Open, FileAccess.Read))
+        {
+            byte[] header = new byte[16];
+            fsInput.ReadExactly(header);
+
+            string str = System.Text.Encoding.ASCII.GetString(header);
+
+            if (str == "SQLite format 3\0")
+            {
+                Console.WriteLine("Resulting file has SQLite header.");
+            }
+            else
+            {
+                Console.WriteLine("Resulting file is missing SQLite header. This could be due to invalid key or differences in encryption method.");
+            }
+        }
+    }
+
+    static void encryptDatabase(string src, string dst, byte[] key)
+    {
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = key;
+            aes.IV = new byte[16];
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.None;
+
+            using (FileStream fsInput = new FileStream(src, FileMode.Open, FileAccess.Read))
+            using (FileStream fsOutput = new FileStream(dst, FileMode.Create, FileAccess.Write))
+            using (ICryptoTransform encryptor = aes.CreateEncryptor())
+            using (CryptoStream cs = new CryptoStream(fsOutput, encryptor, CryptoStreamMode.Write))
+            {
+                fsInput.CopyTo(cs);
+            }
         }
     }
 }
--- archeage.exe-revision-finder/README.md
+++ archeage.exe-revision-finder/README.md
@@ -1,5 +1,20 @@
 # How to find revision from archeage.exe
 
-archeage.exe is built with a custom PE section called `.xlgames` which includes in its first four bytes the integer value of the revision.
+`archeage.exe` is built with a custom PE section called `.xlgames` which includes in its first four bytes the integer value of the revision.
 
 Example C# program included.
+
+## `game.sqlite3` key
+
+`archeage.exe` also appears to include the 128-bit key used to decrypt `game.sqlite3`, which is normally not shipped with the game client.
+Currently observed offset seems to be `.xlgames + 72`, though not rigorously tested between game older versions.
+
+Versions of `game.sqlite3` which surfaced in June 2026 are encrypted using AES-128-CBC with that key, no padding (SQLite database file sizes should be multiples of 16 bytes anyway) and IV=0.
+
+Revised code includes encryption and decryption routines in the example C# program.
+
+Alternatively the key can be pulled from the exe using a hex editor or another reverse engineering tool and database be decrypted using OpenSSL (substitute the key first):
+
+```sh
+openssl aes-128-cbc -d -in game.sqlite3 -out game_decrypted.sqlite3 -K CAFEBABEBEEFCAKEDEAFADDEDFADEDCC -iv 00000000000000000000000000000000 -nopad
+```
